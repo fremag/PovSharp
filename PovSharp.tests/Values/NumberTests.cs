d2afcaf8a1907c51630208dbed6fedda361d940b
fremag <fremag.gh@gmail.com>
2017-09-16 18:13:44 +0200

    Remove Nunit references/code

--- PovSharp.tests/Values/NumberTests.cs
+++ PovSharp.tests/Values/NumberTests.cs
@@ -1,46 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-//using NUnit.Framework;
 using PovSharp.Values;
 
 namespace PovSharp.tests.Values
 {
-#if TOTO
-    [TestFixture]
-    public class NumberTests
-    {
-        [Test]
-        public void TestName()
-        {
-            var n = new PovNumber("x");
-            Assert.That(n.Name, Is.EqualTo("x"));
-        }
-
-        [Test]
-        public void TestValue()
-        {
-            var n = new PovNumber(5);
-            Assert.That(n.Name, Is.Null);
-            Assert.That(n.Value, Is.EqualTo(5));
-        }
-
-        [Test]
-        public void TestClone()
-        {
-            var n = new PovNumber(5);
-            var m = n.Clone() as PovNumber;
-            Assert.That(m.Name, Is.Null);
-            Assert.That(m.Value, Is.EqualTo(5));
-        }
-
-        [Test]
-        public void TestDoubleOperator()
-        {
-            PovNumber n = 5;
-            Assert.That(n.Name, Is.Null);
-            Assert.That(n.Value, Is.EqualTo(5));
-        }
-    }
-#endif
 
     [TestClass]
     public class NumberMSTests
