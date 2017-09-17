1d4a95bafca498331fa71e64cadb82a3348cb793
fremag <fremag.gh@gmail.com>
2017-09-17 17:05:06 +0200

    MSTests: add NFluent

--- PovSharp.tests/Values/NumberTests.cs
+++ PovSharp.tests/Values/NumberTests.cs
@@ -1,9 +1,9 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NFluent;
 using PovSharp.Values;
 
 namespace PovSharp.tests.Values
 {
-
     [TestClass]
     public class NumberTests
     {
@@ -11,15 +11,15 @@ namespace PovSharp.tests.Values
         public void TestName()
         {
             var n = new PovNumber("x");
-            Assert.AreEqual(n.Name, "x");
+            Check.That(n.Name).IsEqualTo("x");
         }
 
         [TestMethod]
         public void TestValue()
         {
             var n = new PovNumber(5);
-            Assert.IsNull(n.Name);
-            Assert.AreEqual(n.Value, 5);
+            Check.That(n.Name).IsNull();
+            Check.That(n.Value).IsEqualTo(5);
         }
 
         [TestMethod]
@@ -27,41 +27,41 @@ namespace PovSharp.tests.Values
         {
             var n = new PovNumber(5);
             var m = n.Clone() as PovNumber;
-            Assert.IsNull(m.Name);
-            Assert.AreEqual(m.Value, 5);
+            Check.That(m.Name).IsNull();
+            Check.That(m.Value).IsEqualTo(5);
         }
 
         [TestMethod]
         public void TestDoubleOperator()
         {
             PovNumber n = 5;
-            Assert.IsNull(n.Name);
-            Assert.AreEqual(n.Value, 5);
+            Check.That(n.Name).IsNull();
+            Check.That(n.Value).IsEqualTo(5);
         }
 
         [TestMethod]
         public void TestPovCode()
         {
             PovNumber n = 5;
-            Assert.AreEqual(n.ToPovCode(), "5");
+            Check.That(n.ToPovCode()).IsEqualTo("5");
             n = 5.2;
-            Assert.AreEqual(n.ToPovCode(), "5.2");
+            Check.That(n.ToPovCode()).IsEqualTo("5.2");
         }
 
         [TestMethod]
         public void TestEquals()
         {
             PovNumber n = 5;
-            Assert.AreEqual(n, 5);
+            Check.That(n).IsEqualTo(5);
         }
         [TestMethod]
         public void TestEquals2()
         {
             PovNumber n = 5;
-            Assert.IsTrue(n.Equals(5.0));
-            Assert.IsTrue(n.Equals(5));
-            Assert.IsTrue(n.Equals(new PovNumber("test", 5)));
-            Assert.IsFalse(n.Equals(new PovNumber("test", 6)));
+            Check.That(n.Equals(5.0)).IsTrue();
+            Check.That(n.Equals(5)).IsTrue();
+            Check.That(n.Equals(new PovNumber("test", 5))).IsTrue();
+            Check.That(n.Equals(new PovNumber("test", 6))).IsFalse();
         }
     }
 }
