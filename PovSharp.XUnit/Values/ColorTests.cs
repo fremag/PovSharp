730852be18e92c6c4414c266ea18182ea2f34dbb
fremag <fremag.gh@gmail.com>
2017-09-24 18:13:40 +0200

    Add Color element + tests

--- PovSharp.XUnit/Values/ColorTests.cs
+++ PovSharp.XUnit/Values/ColorTests.cs
@@ -1,4 +1,5 @@
 using NFluent;
+using PovSharp.Core;
 using PovSharp.Values;
 using Xunit;
 
@@ -24,31 +25,58 @@ namespace PovSharp.XUnit.Values
             Check.That(c.Green).IsEqualTo(1);
             Check.That(c.Blue).IsEqualTo(2);
 
-            Check.That(c.ToPovCode()).IsEqualTo("rgb <0, 1, 2>");
         }
         [Fact]
         public void TestPovColor3()
         {
-            var red = new PovNumber("myRedComp", 0);    
-            var c = new PovColor("myColor") {Red = red, Green = 1.0/2, Blue = 1.0/4};
+            var red = new PovNumber("myRedComp", 0);
+            var c = new PovColor("myColor") { Red = red, Green = 1.0 / 2, Blue = 1.0 / 4 };
             Check.That(c.Name).IsEqualTo("myColor");
             Check.That(c.Red).IsEqualTo(0);
             Check.That(c.Green).IsEqualTo(0.5);
             Check.That(c.Blue).IsEqualTo(0.25);
-
-            Check.That(c.ToPovCode()).IsEqualTo("rgb <myRedComp, 0.5, 0.25>");
         }
         [Fact]
         public void TestPovColor4()
         {
-            var rgb = new PovVector("rgb", 0.1, 0.2, 0.3);    
+            var rgb = new PovVector("rgb", 0.1, 0.2, 0.3);
             var c = new PovColor("myColor", rgb);
             Check.That(c.Name).IsEqualTo("myColor");
             Check.That(c.Red).IsEqualTo(0.1);
             Check.That(c.Green).IsEqualTo(0.2);
             Check.That(c.Blue).IsEqualTo(0.3);
+        }
 
-            Check.That(c.ToPovCode()).IsEqualTo("rgb <0.1, 0.2, 0.3>");
+        [Fact]
+        public void TestPovCode1()
+        {
+            var c = new PovColor();
+            Check.That(c.ToPovCode()).IsEqualTo("rgb <0, 0, 0>");
+        }
+        [Fact]
+        public void TestPovCode2()
+        {
+            var c = new PovColor() {Red = 1, Green = 0, Blue = 0};
+            Check.That(c.ToPovCode()).IsEqualTo("rgb <1, 0, 0>");
+        }
+        [Fact]
+        public void TestPovCode3()
+        {
+            var c = new PovColor(1, 1, 0);
+            Check.That(c.ToPovCode()).IsEqualTo("rgb <1, 1, 0>");
+        }
+        [Fact]
+        public void TestPovCode4()
+        {
+            var c = new PovColor("Black");
+            Check.That(c.ToPovCode()).IsEqualTo("rgb <0, 0, 0>");
+        }
+        [Fact]
+        public void TestPovCode5()
+        {
+            var c = new PovColor("White");
+            var dec = new DeclareElement("myColor", c);
+            Check.That(dec.ToPovCode()).IsEqualTo("#declare myColor = White;");
         }
     }
 }
--- PovSharp/Values/PovColor.cs
+++ PovSharp/Values/PovColor.cs
@@ -4,14 +4,14 @@ namespace PovSharp.Values
 {
     public class PovColor : AbstractPovElement
     {
-        [PovField(0, Before = "rgb <", After=",")]
+        [PovField(0, Before = "rgb <", After = ",")]
         public PovNumber Red { get; set; } = 0;
         [PovField(1, After = ",")]
         public PovNumber Green { get; set; } = 0;
         [PovField(2, After = ">")]
 
         public PovNumber Blue { get; set; } = 0;
-        public PovColor(string name, PovNumber red, PovNumber green,  PovNumber blue  ) : base(name)
+        public PovColor(string name, PovNumber red, PovNumber green, PovNumber blue) : base(name)
         {
             Red = red;
             Green = green;
@@ -20,23 +20,43 @@ namespace PovSharp.Values
         public PovColor(string name) : base(name)
         {
         }
-        public PovColor(string name, double red, double green, double blue  ) : this(name, new PovNumber(red), new PovNumber(green), new PovNumber(blue))
-        {}
+        public PovColor(string name, double red, double green, double blue) : this(name, new PovNumber(red), new PovNumber(green), new PovNumber(blue))
+        { }
 
-        public PovColor(double red, double green, double blue  ) : this(null, red, green, blue)
-        {}
-        public PovColor(PovNumber red, PovNumber green, PovNumber blue  ) : this(null, red, green, blue)
-        {}
+        public PovColor(double red, double green, double blue) : this(null, red, green, blue)
+        { }
+        public PovColor(PovNumber red, PovNumber green, PovNumber blue) : this(null, red, green, blue)
+        { }
         public PovColor(string name, PovVector rgb) : this(name, rgb.X, rgb.Y, rgb.Z)
-        {}
+        { }
 
         public PovColor() : base(null)
         {
         }
 
-        public override string ToPovCode()
+        public override string ToPovCode() => this.BuildPovCode();
+        public override string ToString() => $"{Name ?? "NoName"}: < {Red}, {Green}, {Blue}>";
+
+        public override bool Equals(object obj)
         {
-            return this.BuildPovCode();
+            var other = obj as PovColor;
+            if (other == null)
+            {
+                return false;
+            }
+            bool b = (other.Red == Red);
+            b &= (other.Green == Green);
+            b &= (other.Blue == Blue);
+            return b;
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 17;
+            hash *= 23 + Red.GetHashCode();
+            hash *= 23 + Green.GetHashCode();
+            hash *= 23 + Blue.GetHashCode();
+            return hash;
         }
     }
 }
