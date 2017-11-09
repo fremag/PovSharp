7e4c5096411e9e5e789d8592a6520dbcd01168d3
fremag <fremag.gh@gmail.com>
2017-11-09 20:49:24 +0100

    SurfaceOfRevolution: fix unit test

--- PovSharp.XUnit/Objects/SurfaceOfRevolutionTests.cs
+++ PovSharp.XUnit/Objects/SurfaceOfRevolutionTests.cs
@@ -39,7 +39,7 @@ namespace PovSharp.XUnit.Objects
                 .Add(1, 1).Add(5, 1).Add(5, 7).Add(1, 7).Add(1, 1);   //inner rim    
 
             var povCode = sor.ToPovCode();
-            Check.That(povCode).IsEqualTo("sor {\n bezier_spline\n 10\n < 0, 0>, < 6, 0>, < 6, 8>, < 0, 8>, < 0, 0>, < 1, 1>, < 5, 1>, < 5, 7>, < 1, 7>, < 1, 1>\n open\n sturm\n}");
+            Check.That(povCode).IsEqualTo("sor {\n 10\n < 0, 0>, < 6, 0>, < 6, 8>, < 0, 8>, < 0, 0>, < 1, 1>, < 5, 1>, < 5, 7>, < 1, 7>, < 1, 1>\n open\n sturm\n}");
         }
     }
 }
