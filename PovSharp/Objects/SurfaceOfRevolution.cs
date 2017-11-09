73dc4f9402bb8eab8e40e1e67d6eba73e376da85
fremag <fremag.gh@gmail.com>
2017-11-09 15:41:01 +0100

    SurfaceOfRevolution: removed wrong spline type field

--- PovSharp/Objects/SurfaceOfRevolution.cs
+++ PovSharp/Objects/SurfaceOfRevolution.cs
@@ -6,19 +6,15 @@ namespace PovSharp.Objects
     public class SurfaceOfRevolution : AbstractPovObject
     {
         [PovField(0, After = "\n")]
-        public SplineType? Spline { get; set; } = null;
-        
-
-        [PovField(1, After = "\n")]
         public PovNumber NbPoints => Points.Count;
 
-        [PovField(2, After = "\n")]
+        [PovField(1, After = "\n")]
         private PovList<Point2D> Points { get; set; } = new PovList<Point2D>();
 
-        [PovField(3, After = "\n")]
+        [PovField(2, After = "\n")]
         public OpenFlag? Open {get; set;} 
         
-        [PovField(4, After = "\n")]
+        [PovField(3, After = "\n")]
         public SturmFlag? Sturm {get; set;} 
 
         public SurfaceOfRevolution() : base()
