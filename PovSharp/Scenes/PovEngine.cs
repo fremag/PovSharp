94f38da764e5c640b63826168ecc7076d85c299c
fremag <fremag.gh@gmail.com>
2017-11-15 17:07:13 +0100

    PovEngine: basic format/indent when generating scene file

--- PovSharp/Scenes/PovEngine.cs
+++ PovSharp/Scenes/PovEngine.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -22,22 +23,43 @@ namespace PovSharp.Scenes
         string ExePath { get; } = "";
         string SceneExportPath { get; } = Path.GetTempPath();
 
-        public (string, Process) Render(PovScene scene, PovEngineOptions options, bool exitWhenDone=true)
+        public (string, Process) Render(PovScene scene, PovEngineOptions options, bool exitWhenDone = true)
         {
             var sceneFile = Path.ChangeExtension(scene.Name, PovExtension);
-            var filePath =  Path.Combine(SceneExportPath, sceneFile);
+            var filePath = Path.Combine(SceneExportPath, sceneFile);
 
-            File.WriteAllLines(filePath, scene.ToPovCode());
+            File.WriteAllLines(filePath, Indent(scene.ToPovCode()));
 
             var args = new List<string>(options.GetPovArgs());
-            args.Add($"Input_File_Name=\"{filePath}\"") ;
-            if( exitWhenDone ) 
+            args.Add($"Input_File_Name=\"{filePath}\"");
+            if (exitWhenDone)
             {
                 args.Add("/exit");
             }
             var process = Process.Start(ExePath, string.Join(" ", args));
-            
+
             return (filePath, process);
+        }
+
+        public static IEnumerable<string> Indent(IEnumerable<string> povCodes)
+        {
+            int idx = 0;
+            foreach (var povCode in povCodes)
+            {
+                string[] lines = povCode.Split("\n");
+                foreach (var line in lines)
+                {
+                    var trimLine = line.TrimStart();
+                    if(string.IsNullOrEmpty(trimLine)) 
+                    {
+                        continue;
+                    }
+                    idx -= trimLine.Count(f => f == '}');
+                    var newLine = string.Concat(Enumerable.Repeat("\t", idx).Append(trimLine));
+                    idx += trimLine.Count(f => f == '{');
+                    yield return newLine;
+                }
+            }
         }
     }
 }
